005dfc44ade2b2f1f0b1e2f9d939af0d37542cae
SosaMati <matiasezequielsosa@gmail.com>
2022-07-13 02:36:07 -0300

    proyecto cotillon agregado

--- src/data.js
+++ src/data.js
@@ -1,36 +1,36 @@
 import Journal from '../src/img/journal.png';
 import Heroes from '../src/img/heroes.png';
 import Gift from '../src/img/gift.png';
+import Cotillon from '../src/img/cotillon.png';
 
 
 export const products = [
     {
         id: 1,
+        img: Cotillon,
+        link: 'https://lahormiguitaviajera-self.vercel.app/',
+        repo: 'https://github.com/SosaMati/cotillon'
+    },
+    {
+        id: 2,
         img: Journal,
         link: 'https://journal-app-five.vercel.app/',
         repo: 'https://github.com/SosaMati/JournalApp'
     },
 
     {
-        id: 2,
+        id: 3,
         img: Heroes,
         link: 'https://heroes-app-psi.vercel.app/',
         repo: 'https://github.com/SosaMati/Heroes-App'
     },
 
     {
-        id: 3,
+        id: 4,
         img: Gift,
         link: 'https://react-giftsearchapp.vercel.app/',
         repo: 'https://github.com/SosaMati/react-giftsearchapp'
     },
-
-    {
-        id: 4,
-        img: '',
-        link: ''
-    },
-
     {
         id: 5,
         img: '',
