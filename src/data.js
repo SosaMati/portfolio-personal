1525654a1a7756ca3b32611394fb129d1aa1958d
SosaMati <matiasezequielsosa@gmail.com>
2022-07-27 02:53:05 -0300

    agregado de calendarApp

--- src/data.js
+++ src/data.js
@@ -2,41 +2,40 @@ import Journal from '../src/img/journal.png';
 import Heroes from '../src/img/heroes.png';
 import Gift from '../src/img/gift.png';
 import Cotillon from '../src/img/cotillon.png';
+import Calendar from '../src/img/calendar.png';
 
 
 export const products = [
     {
         id: 1,
+        img: Calendar,
+        link: 'https://mern-calendar-mati.herokuapp.com/',
+        repo: 'https://github.com/SosaMati/mern-calendar'
+    },
+    {
+        id: 2,
         img: Cotillon,
         link: 'https://lahormiguitaviajera-self.vercel.app/',
         repo: 'https://github.com/SosaMati/cotillon'
     },
     {
-        id: 2,
+        id: 3,
         img: Journal,
         link: 'https://journal-app-nu-eight.vercel.app/',
         repo: 'https://github.com/SosaMati/JournalApp'
     },
-
     {
-        id: 3,
+        id: 4,
         img: Heroes,
         link: 'https://heroes-app-psi.vercel.app/',
         repo: 'https://github.com/SosaMati/Heroes-App'
     },
-
     {
-        id: 4,
+        id: 5,
         img: Gift,
         link: 'https://react-giftsearchapp.vercel.app/',
         repo: 'https://github.com/SosaMati/react-giftsearchapp'
     },
-    {
-        id: 5,
-        img: '',
-        link: ''
-    },
-
     {
         id: 6,
         img: '',
