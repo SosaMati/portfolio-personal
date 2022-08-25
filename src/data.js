19710b6da1c793f29c1db1162cf4dbbcddd62735
SosaMati <matiasezequielsosa@gmail.com>
2022-08-25 00:39:49 -0300

    Presupuesto App agregado

--- src/data.js
+++ src/data.js
@@ -3,49 +3,53 @@ import Heroes from '../src/img/heroes.webp';
 import Gift from '../src/img/gift.webp';
 import Cotillon from '../src/img/cotillon.webp';
 import Calendar from '../src/img/calendar.webp';
+import Presupuesto from '../src/img/presupuesto.webp';
+import Veterinaria from '../src/img/veterinaria.webp';
+
 
 
 export const products = [
     {
         id: 1,
+        img: Presupuesto,
+        title:'Presupuesto App',
+        link: 'https://control-presupuesto-neon.vercel.app/',
+        repo:'https://github.com/SosaMati/control-presupuesto'
+    },
+    {
+        id: 2,
         img: Calendar,
         title: 'Calendar App',
         link: 'https://mern-calendar-mati.herokuapp.com/',
         repo: 'https://github.com/SosaMati/mern-calendar'
     },
     {
-        id: 2,
+        id: 3,
         img: Cotillon,
         title: 'Cotillón App',
         link: 'https://lahormiguitaviajera-self.vercel.app/',
         repo: 'https://github.com/SosaMati/cotillon'
     },
     {
-        id: 3,
+        id: 4,
         img: Journal,
         title: 'Journal App',
-        link: 'https://journal-app-nu-eight.vercel.app/',
+        link: 'https://journal-app-beta-puce.vercel.app/',
         repo: 'https://github.com/SosaMati/JournalApp'
     },
     {
-        id: 4,
+        id: 5,
         img: Heroes,
         title: 'Heroes App',
         link: 'https://heroes-app-psi.vercel.app/',
         repo: 'https://github.com/SosaMati/Heroes-App'
     },
     {
-        id: 5,
+        id: 6,
         img: Gift,
         title: 'Gifs Search App',
         link: 'https://react-giftsearchapp.vercel.app/',
         repo: 'https://github.com/SosaMati/react-giftsearchapp'
-    },
-    {
-        id: 6,
-        img: '',
-        title:'',
-        link: '',
-        repo:''
-    },
+    }
+   
 ]
